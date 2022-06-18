fabf7d089444faf6b6cfc235219f253af031300a
Aleksandr <soft@vhlv.ru>
2022-06-18 14:37:30 +0300

    refactor: add condition

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,20 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './_Section.scss';
 
 function Section(props) {
   return (
     <section className={`section ${props.name}`} id={`${props.name}`}>
-      <article className={`section__info ${props.name}__info`}>
-        <h2 className={`section__title ${props.name}__title`}>{props.title}</h2>
-        {props.children}
-      </article>
+      {props.name === 'promo' ? (
+        <>
+          <img
+            className={`${props.name}__image`}
+            src={props.image}
+            alt={props.title}
+          />
+          <article className={`section__info ${props.name}__info`}>
+            <h1 className={`section__title ${props.name}__title`}>
+              {props.title}
+            </h1>
+            {props.children}
+          </article>
+        </>
+      ) : (
+        <article className={`section__info ${props.name}__info`}>
+          <h2 className={`section__title ${props.name}__title`}>
+            {props.title}
+          </h2>
+          {props.children}
+        </article>
+      )}
     </section>
   );
 }
 export default Section;
 
 Section.propTypes = {
-  name: Section.string,
-  title: Section.string,
-  children: Section.isRequired,
+  name: PropTypes.string,
+  image: PropTypes.string,
+  title: PropTypes.string,
+  children: PropTypes.node,
 };
