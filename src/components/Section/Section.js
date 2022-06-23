7949ec43b526b101d580d03a0774a36c39fc8b33
Aleksandr <soft@vhlv.ru>
2022-06-23 22:51:22 +0300

    refactor: remove condition

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -4,19 +4,11 @@ import './_Section.scss';
 
 function Section(props) {
   return (
-    <>
-      {props.name === 'promo' ? (
-        <section className={`section ${props.name}`} id={`${props.id}`}>
-          {props.children}
-        </section>
-      ) : (
-        <section
-          className={`section ${props.theme ? 'section_light ' : ''}${props.name}`}
-          id={`${props.id}`}>
-          {props.children}
-        </section>
-      )}
-    </>
+    <section
+      className={`section ${props.theme ? 'section_light ' : ''}${props.name}`}
+      id={`${props.id}`}>
+      {props.children}
+    </section>
   );
 }
 export default Section;
@@ -25,7 +17,5 @@ Section.propTypes = {
   id: PropTypes.string,
   theme: PropTypes.bool,
   name: PropTypes.string,
-  image: PropTypes.string,
-  title: PropTypes.string,
   children: PropTypes.node,
 };
