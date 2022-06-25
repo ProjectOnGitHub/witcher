7bfba95c593d7aefd6d17ac35db71aac8ef7ff19
Aleksandr <soft@vhlv.ru>
2022-06-25 20:28:14 +0300

    Updates

--- src/components/Footer/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './_Footer.scss';
-
-function Footer() {
-  return <footer className='footer'>Footer</footer>;
-}
-
-export default Footer;
--- src/components/MainPrime/MainPrime.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Main from '../Main/Main';
-import './_MainPrime.scss';
-
-function MainPrime() {
-  return <Main />;
-}
-
-export default MainPrime;
