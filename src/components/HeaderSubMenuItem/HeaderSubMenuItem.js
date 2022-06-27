422646dc10404098a324dbd1d4643aa30b83a7c3
Aleksandr <soft@vhlv.ru>
2022-06-27 16:14:11 +0300

    refactor: replace a href

--- src/components/HeaderSubMenuItem/HeaderSubMenuItem.js
+++ src/components/HeaderSubMenuItem/HeaderSubMenuItem.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './_HeaderSubMenuItem.scss';
 
 function HeaderSubMenuItem(props) {
   return (
     <li className='header__submenu-item'>
-      <a href={`${props.url}#${props.id}`} className='header__link'>
+      <Link to={`${props.url}#${props.id}`} className='header__link'>
         {props.heading}
-      </a>
+      </Link>
     </li>
   );
 }
